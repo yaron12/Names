83054ef2032f28e7d1c1302f690e772e7d46a78c
yaronbensimon1@gmail.com <026535898>
2016-07-29 11:30:50 +0300

    names

---
+++ names.js
@@ -0,0 +1,32 @@
+var name1,name2;
+var counter=0;
+name1="yaron";
+name2="yotan";
+if(name1[0]==name2[0])
+{
+counter=counter+1; 
+};
+if(name1[1]==name2[1])
+{
+counter=counter+1; 
+};
+if(name1[2]==name2[2])
+{
+counter=counter+1; 
+};
+if(name1[3]==name2[3])
+{
+counter=counter+1; 
+};
+if(name1[4]==name2[4])
+{
+counter=counter+1;
+};
+if (counter>=2)
+{
+	alert ("There is" + (counter) + "chart much")
+	}
+	else
+	{
+		alert (false)
+	}
